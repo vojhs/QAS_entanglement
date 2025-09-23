784c3034235640f6c71d457962b1a1f6f3bd7a09
Resonance <136827237+vojhs@users.noreply.github.com>
2025-09-23 23:13:47 +0800

    Update qas_env.py

--- src/qas_gym/envs/qas_env.py
+++ src/qas_gym/envs/qas_env.py
@@ -1,3 +1,23 @@
+# QAS-entanglement
+# Copyright (C) 2024 [vojhs]
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+# This file contains modifications based on the project "quantum-arch-search",
+# which is also licensed under GPLv3. The original project can be found at:
+# https://github.comcom/qdevpsi3/quantum-arch-search
+
 import sys
 from contextlib import closing
 from io import StringIO
@@ -850,4 +870,5 @@ class QuantumArchSearchEnv(gym.Env):
         rho_target = cirq.density_matrix_from_state_vector(self.target)
         rho_target = (rho_target + rho_target.conj().T) / 2
         s_tot_norm_tar, s_a_norm_tar, s_b_norm_tar = self.calculate_system_and_subsystem_renyi(rho_target, len(self.qubits), 2)
+
         return s_tot_norm_tar, s_a_norm_tar, s_b_norm_tar
